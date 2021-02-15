8d09946588be8219cad254775e4fdc290d51f920
Daniel Godday <dtrustarscientist@gmail.com>
2021-02-15 21:41:51 +0100

    Go Live Now,Pls

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
-const http=require("http");
-const fs=require("fs");
-//how to get files
-function getFile(fileName){
-	fs.readFile(fileName,(err,data)=>{
-		if(err){
-			return 0;
-		}
-		return data;
-
-	});
-}
-const server=http.createServer((req,res)=>{
-	let url=req.url;
-	let responseData='';
-
-	//check what to do with the URLs 
-	if(url==="/"){
-		responseData=getFile("index.html");
+//get external dependencies
+const {createServer}=require("http");
+const {readFile}=require("fs");
+const PORT=8080;
+//create a server object
+let server=createServer((req,res)=>{
+	//parse the URLs
+	let file4Url=req.url.substring(1).toLowerCase();
+	//test paths
+	if(file4Url=="index" || file4Url==""){
+		readFile("index.html",{encoding:"utf8"},(err,data)=>{
+			//check file 
+			if(err){
+				res.writeHead(404,{"Content-Type":"text/plain"});
+				res.write("not found : "+file4Url);
+				res.end();
+			}
+			else{
+				res.writeHead(200,{"Content-Type":"text/html"});
+				res.write(data);
+				res.end();
+			}
+		});
 	}
-	else if(url==="/about"){
-		responseData("about.html");
+	else if(file4Url=="about"){
+		readFile("about.html",{encoding:"utf8"},(err,data)=>{
+			//check file 
+			if(err){
+				res.writeHead(404,{"Content-Type":"text/plain"});
+				res.write("not found : "+file4Url);
+				res.end();
+			}
+			else{
+				res.writeHead(200,{"Content-Type":"text/html"});
+				res.write(data);
+				res.end();
+			}
+		});
 	}
-	else if(url==="/contact-me"){
-		responseData=getFile("contact-me.html");
-	}
+	else if(file4Url=="contact-me"){
+		readFile("contact-me.html",{encoding:"utf8"},(err,data)=>{
+			//check file 
+			if(err){
+				res.writeHead(404,{"Content-Type":"text/plain"});
+				res.write("not found : "+file4Url);
+				res.end();
+			}
+			else{
+				res.writeHead(200,{"Content-Type":"text/html"});
+				res.write(data);
+				res.end();
+			}
+		});
+	} 
 	else{
-		responseData=getFile("404.html");
+		readFile("404.html",{encoding:"utf8"},(err,data)=>{
+			//check file 
+			if(err){
+				res.writeHead(404,{"Content-Type":"text/plain"});
+				res.write("not found "+file4Url);
+				res.end();
+			}
+			else{
+				res.writeHead(200,{"Content-Type":"text/html"});
+				res.write(data);
+				res.end();
+			}
+		});
 	}
-
-	//then respond to request
-	res.writeHead(200,{"Content-Type":"text/html"});
-	res.write(responseData);
-	res.end();
 });
-server.listen(5500);
+
+
+//server listening port
+
+server.listen(PORT); 
+console.log("listening! at port "+PORT);
